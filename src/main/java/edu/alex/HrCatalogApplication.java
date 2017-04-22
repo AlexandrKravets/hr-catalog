2dfa83c7e16198c62666cf1b43b7493ad55f3f3f
rostislav.dudka <rostislav.dudka@gmail.com>
2017-04-22 17:39:39 +0300

    asdsad

--- src/main/java/edu/alex/HrCatalogApplication.java
+++ src/main/java/edu/alex/HrCatalogApplication.java
@@ -21,7 +21,7 @@ public class HrCatalogApplication {
         return new CommandLineRunner() {
             @Override
             public void run(String... strings) throws Exception {
-                Group group = new Group("Test");
+                Group group = new Group("Tesapodspaojdst");
                 Contact contact;
 
                 contactService.addGroup(group);
