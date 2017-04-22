19dd36e8c4a83c240edf56bb2774d31e207bf379
rostislav.dudka <rostislav.dudka@gmail.com>
2017-04-22 17:42:08 +0300

    -last dev commit;

--- src/main/java/edu/alex/config/ApplicationConfiguration.java
+++ src/main/java/edu/alex/config/ApplicationConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories(basePackages = "edu.alex.model.repository")
 public class ApplicationConfiguration {
-
+/*
+dkfdkjffjdfj
+ */
 
 }
